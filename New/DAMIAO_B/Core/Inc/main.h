053574e745c3d0cf243797cc4ea8d0974d61e1c6
CandyLOVO <1565630950@qq.com>
2024-03-01 22:20:55 +0800

    使能5V、24V电源接口

--- New/DAMIAO_B/Core/Inc/main.h
+++ New/DAMIAO_B/Core/Inc/main.h
@@ -57,8 +57,14 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define Power1_Pin GPIO_PIN_13
+#define Power1_GPIO_Port GPIOC
+#define OUT_5V_Pin GPIO_PIN_14
+#define OUT_5V_GPIO_Port GPIOC
 #define LED_Pin GPIO_PIN_0
 #define LED_GPIO_Port GPIOC
+#define Power2_Pin GPIO_PIN_4
+#define Power2_GPIO_Port GPIOC
 
 /* USER CODE BEGIN Private defines */
 
