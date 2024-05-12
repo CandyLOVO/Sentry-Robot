31fd8359a286178d973f0b9d675114400080c8e9
CandyLOVO <1565630950@qq.com>
2024-05-13 01:13:21 +0800

    开始调重力补偿之后的Pitch

--- NewNew/SB_UP/USER/Task/Pitch_task.c
+++ NewNew/SB_UP/USER/Task/Pitch_task.c
@@ -124,12 +124,12 @@ void Pitch_Task(void * argument)
 //**************************************************代码实现**************************************************//
 static void Pitch_init(void)
 {
-	initial_pitch_L = 8171;
+	initial_pitch_L = 2742;
 	initial_pitch_R = 4000;
 	
 	//PID初始化
-	pid_init(&pid_pitch_s_L,170,0.3,0,16384,16384); //PID初始化 PI
-	pid_init(&pid_pitch_a_L,20,0,30,16384,16384); //PD
+	pid_init(&pid_pitch_s_L,0,0,0,16384,16384); //PID初始化 PI
+	pid_init(&pid_pitch_a_L,0,0,0,16384,16384); //PD
 	
 	pid_init(&pid_pitch_s_R,170,0.3,0,16384,16384); //PID初始化
 	pid_init(&pid_pitch_a_R,20,0,30,16384,16384);
